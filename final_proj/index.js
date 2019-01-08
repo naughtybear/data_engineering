66b214758ba165a9dfb95c33d3373ebc3f29e6da
naughtybear <simon86521@gmail.com>
2019-01-08 15:29:33 +0800

    new web

--- final_proj/index.js
+++ final_proj/index.js
@@ -3,47 +3,96 @@ var app = express()
 var exec = require('child_process').exec
 var fs = require('fs')
 
+const NEWS_PER_PAGE = 10
+
 function isNAN(obj) {
     //return !(obj === obj);
     if(!obj) return 1;
 }
+
+app.use('/view', express.static('view'))
 
 app.get('/', function (req, res){
     res.sendFile(__dirname + '/index.html')
 })
 
 app.get('/Search', function(req,res){
-    console.log(req.query.search)
+    console.log(req.query)
+    //console.log(req.query.ifsearch)
+    res.header('Access-Control-Allow-Origin', '*');
+    let page = parseInt(req.query.page)
     //res.send('just test')
-    exec('./dp_search ettoday0.rec -p "'+ req.query.search +'"', (err, stdout) => {
-        //if(err) throw err;
+    if(req.query.ifsearch == 'true'){
+        console.log("111")
+        exec('./dp_search ettoday0.rec -p "'+ req.query.search +'"', (err, stdout) => {
+            //if(err) throw err;
+            fs.readFile(__dirname + '/out2.txt', (err, data) => {
+                //res.send('hahaha')
+                if(err) throw err;
+                //console.log(typeof(data))
+                data = String(data).split('\n')
+                let maxPage = Math.ceil((data.length-1) / NEWS_PER_PAGE)
+                let array = []
+                for(let i=page*NEWS_PER_PAGE; i<Math.min((page+1)*NEWS_PER_PAGE, data.length-1); i++){
+                    let buf = data[i].split('\t')
+                    let tmp = {
+                        "id" : i,
+                        "score" : buf[0],
+                        "url" : buf[1],
+                        "title" : buf[2],
+                        "content" : buf[3].substring(0,200),
+                        "maxPage" : maxPage
+                    }
+                    array.push(tmp)
+                }
+                //res.end ("</table></div>");
+                res.send(array)
+            })
+        })
+    }
+    else{
+        console.log("222")
         fs.readFile(__dirname + '/out2.txt', (err, data) => {
             //res.send('hahaha')
             if(err) throw err;
             //console.log(typeof(data))
             data = String(data).split('\n')
-            res.write('<head><meta charset="utf-8"/></head>');
-            res.write('<form action="http://127.0.0.1:3000/Search" method="GET">Search: <input type="text" name="search"> <input type="submit" value="Submit"></form>')
-            res.write('<div> <table  width=1000 border=1 >');
-            for(let i=0; i<data.length; i++){
+            let maxPage = Math.ceil((data.length-1) / NEWS_PER_PAGE)
+            let array = []
+            for(let i=page*NEWS_PER_PAGE; i<Math.min((page+1)*NEWS_PER_PAGE, data.length-1); i++){
                 let buf = data[i].split('\t')
-                if(isNAN(buf[1]))
-                    continue;
-                res.write('<tr>')
-                res.write('<td><a href='+buf[1]+'>'+buf[2]+'</a></td>')
-                res.write('<td>' + buf[0] + '</td>') 
-                res.write('<td>' + buf[3] + '</td>') 
+                let tmp = {
+                    "id" : i,
+                    "score" : buf[0],
+                    "url" : buf[1],
+                    "title" : buf[2],
+                    "content" : buf[3].substring(0,200),
+                    "maxPage" : maxPage
+                }
+                array.push(tmp)
             }
-            res.end ("</table></div>");
+            //res.end ("</table></div>");
+            res.send(array)
         })
-        //res.send(stdout)
-    })
+    }
 })
 
 app.post('/haha', function(req, res){
     res.send("you are idiot")
 })
 
+app.get('/test', function(req, res){
+    console.log(req.query.id)
+    tmp = ["aaa", "bbb", "ccc"]
+    res.header('Access-Control-Allow-Origin', '*');
+    res.set('Content-Type', 'text/html');
+    res.send(req.query.id);
+})
+
+
+app.get('/test2', function (req, res){
+    res.sendFile(__dirname + '/test.html')
+})
 
 var server = app.listen(3000, function(){
     console.log ('Server跑起來了，現在時間是:' + new Date())
--- final_proj/view/test.js
+++ final_proj/view/test.js
@@ -2,15 +2,70 @@ const url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsy
 const url2 = "http://localhost:3000/test"
 const url3 = "http://localhost:3000/Search"
 
-new Vue({
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return decodeURIComponent(pair[1]);}
+       }
+       return(false);
+}
+
+let vm =new Vue({
     el: '#app',
     data: {
-        array: []
+        array: null,
+        maxpage : null
     },
 
     created() {
         axios
-        .get(url3, {params: {search: "+英雄聯盟, -Garena"}})
+        .get(url3, {params: {search: getQueryVariable('search'), ifsearch: true, page: 0}})
+        .then(response => {
+            this.array = response.data
+            this.maxpage = response.data[0].maxPage   
+        })
+
+    },
+    
+    methods: {
+        alerttest: function(msg){
+            alert(msg)
+            alert(getQueryVariable('search'))
+        },
+        sendmsg: function(msg){
+            axios
+            .get(url3, {params: {search: getQueryVariable('search'), ifsearch: false, page: msg-1}})
+            .then(response => {
+                this.array = response.data
+                this.maxpage = response.data[0].maxPage   
+            })
+        }
+    },
+})
+
+/*new Vue({
+    el: '#app2',
+    data: {
+        array: null
+    },
+
+    created() {
+        axios
+        .get(url3, {params: {search: "+英雄聯盟, -Garena", ifsearch: true}})
         .then(response => (this.array = response.data))
     }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+})*/
+
+Vue.component('news-test', {
+    props : ['news'],
+    template :  `
+        <div class="news">
+            <h4 class="row"><a :href= "news.url" class="col-8">{{ news.title }}</a>  <small class="col">score:{{ news.score }}</small></h4>
+            <h6><a :href= "news.url" >{{ news.url }}</a></h6>
+            <h6>{{ news.content }}</h6>
+        </div>
+    `
 })
